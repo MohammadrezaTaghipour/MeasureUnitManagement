cdca09c64f74337c446ce739afbf8f24a23a3c02
MohammadReza <mohamadrezataghipour73@gmail.com>
2020-03-10 17:33:14 +0330

    add MeasureDimensionQueryHandler

---
+++ Code/Src/MeasureUnitManagement/Infrastructure/Persistence/MeasureDimensionRepository.cs
@@ -0,0 +1,30 @@
+﻿using MeasureUnitManagement.Domain.MeasureDimensions;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MeasureUnitManagement.Infrastructure.Persistence
+{
+    public class MeasureDimensionRepository : IMeasureDimensionRepository
+    {
+        public MeasureDimensionRepository()
+        {
+
+        }
+
+        public Task Add(MeasureDimension measureDimension, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<MeasureDimension> GetById(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<long> GetNextId()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Code/Src/MeasureUnitManagement/Infrastructure/Query/Handlers/MeasureDimensionQueryHandler.cs
@@ -0,0 +1,17 @@
+﻿using MeasureUnitManagement.Infrastructure.Query.Models;
+using MediatR;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MeasureUnitManagement.Infrastructure.Query.Handlers
+{
+    public class MeasureDimensionQueryHandler :
+        IRequestHandler<GetMeasureDimensionById, MeasureDimensionResponse>
+    {
+        public Task<MeasureDimensionResponse> Handle(GetMeasureDimensionById request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
